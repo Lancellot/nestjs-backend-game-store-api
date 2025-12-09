737eed42b7d313f27ea8be8ba0e307652914078e
Lancellot <assis.neto@rede.ulbra.br>
2025-12-09 11:40:01 -0300

    feat: implementação do service do module categoria

--- src/categoria/services/categoria.service.ts
+++ src/categoria/services/categoria.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { Categoria } from "../entities/categoria.entity";
+import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
-export class CategoriaService {}
+export class CategoriaService {
+    constructor(
+        @InjectRepository(Categoria)
+        private categoriaRepository: Repository<Categoria>,
+    ) {}
+
+    async findAll(): Promise<Categoria[]> {
+        return this.categoriaRepository.find({
+            relations: {
+                jogos: true,
+            },
+        });
+    }
+
+    async findById(id: number): Promise<Categoria> {
+        const categoria = await this.categoriaRepository.findOne({
+            where: { id },
+            relations: {
+                jogos: true,
+            },
+        });
+        if (!categoria) {
+            throw new HttpException(`Categoria with ID ${id} not found`, HttpStatus.NOT_FOUND);
+        }
+        return categoria;
+    }
+
+    async create(categoria: Categoria): Promise<Categoria> {
+        return this.categoriaRepository.save(categoria);
+    }
+
+    async update(categoria: Categoria): Promise<Categoria> {
+        const existingCategoria = await this.categoriaRepository.findOne({
+            where: { id: categoria.id },
+        });
+        if (!existingCategoria) {
+            throw new HttpException(`Categoria with ID ${categoria.id} not found`, HttpStatus.NOT_FOUND);
+        }
+        return this.categoriaRepository.save(categoria);
+    }
+
+    async delete(id: number): Promise<void> {
+        const result = await this.categoriaRepository.delete(id);
+        if (result.affected === 0) {
+            throw new Error(`Categoria with ID ${id} not found`);
+        }
+    }
+
+}
