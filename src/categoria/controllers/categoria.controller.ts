179deb363f90c80727125a03635a1fe3ee8dea90
Lancellot <assis.neto@rede.ulbra.br>
2025-12-09 11:54:00 -0300

    feat: congiruando method decorator de routas

--- src/categoria/controllers/categoria.controller.ts
+++ src/categoria/controllers/categoria.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from "@nestjs/common";
+import { 
+    Controller, 
+    Delete, 
+    Get, 
+    HttpCode, 
+    HttpStatus, 
+    Post, 
+    Put 
+} from "@nestjs/common";
 import { CategoriaService } from "../services/categoria.service";
 
 @Controller('/categoria')
@@ -7,26 +15,38 @@ export class CategoriaController {
         private readonly categoriaService: CategoriaService
     ) {}
 
+    @Get()
+    @HttpCode(HttpStatus.OK)
     findAll() {
         return this.categoriaService.findAll();
     }
 
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
     findById(id: number) {
         return this.categoriaService.findById(id);
     }
 
+    @Get('/descricao/:descricao')
+    @HttpCode(HttpStatus.OK)
     findByDescricao(descricao: string) {
         return this.categoriaService.findByDescricao(descricao);
     }
 
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
     create(categoria: any) {
         return this.categoriaService.create(categoria);
     }
 
+    @Put()
+    @HttpCode(HttpStatus.OK)
     update(categoria: any) {
         return this.categoriaService.update(categoria);
     }
 
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
     delete(id: number) {
         return this.categoriaService.delete(id);
     }
